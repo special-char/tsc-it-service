60c17871369430bdef8c9cc5896017b75d50abf0
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-03-10 15:00:59 +0530

    commit code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,14 @@ import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Test from "@/components/Button";
 import ServiceCard from "@/components/ServiceCard/ServiceCard";
+import TestimonialCard from "@/components/TestimonialCard/TestimonialCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <TestimonialCard />
+    </>
+  );
 }
--- src/components/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./style.css";
+import Review from "../Review/Review";
+import Author from "../Author/Author";
 
 const TestimonialCard = () => {
-  return <div>TestimonialCard</div>;
+  return (
+    <div className="slide">
+      <div className="slide_content">
+        <div className="client_wrapper">
+          <Review />
+          <p className="quote_text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta,
+            laudantium.
+          </p>
+          <Author />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialCard;
