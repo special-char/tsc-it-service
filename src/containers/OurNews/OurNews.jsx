0d7f0edfea5122d7f6a455274bd300c09b35cf1a
Umang Patel <umang.patel2014@gmail.com>
2023-03-21 18:24:41 +0530

    did some pixel perfect things

--- src/containers/OurNews/OurNews.jsx
+++ src/containers/OurNews/OurNews.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const OurNews = () => {
   return (
-    <div className="grid grid-rows-2 gap-7 lg:grid-cols-2 lg:grid-rows-1 lg:gap-60">
-      <div className="right_content">
-        <h1 className="text-9xl lg:text-[170px]">
+    <div className="flex flex-col lg:flex-row lg:gap-12 pb-4 lg:min-h-screen">
+      <div className="right_content flex pt-12 xl:pt-16">
+        <h1 className="text-9xl lg:text-[120px] xl:text-[170px]">
           Our <br /> <span className="text-gray"> News </span>
         </h1>
       </div>
-      <div className="left_content grid grid-row content-center gap-7">
+      <div className="left_content flex flex-col gap-8 justify-center pb-10 xl:pl-48">
         <p className="text-dimGray text-xxl">
           We share our thoughts on design. We design awesome things.
         </p>
         <div>
-          <button className="btn">Work With Us</button>
+          <button className="btn ">Work With Us</button>
         </div>
       </div>
     </div>
--- src/containers/aboutScreen/AboutScreen.jsx
+++ src/containers/aboutScreen/AboutScreen.jsx
@@ -4,23 +4,23 @@ const About = () => {
   return (
     <div className="about">
       <div className="container relative z-[1] mb-20 mx-auto max-w-[300px] sm:max-w-[355px] md:max-w-sm lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl flex flex-col flex-wrap w-full">
-        <div className="content flex flex-col gap-20 lg:flex-row">
+        <div className="content flex flex-col gap-2 xl:flex-row ">
           <div>
             <img
-              className="image aspect-auto w-[600px] lg:w-[500px]"
+              className="image aspect-auto flex-1 "
               src="https://assets.website-files.com/6315d6de2357050021f26e96/63187ca7418405f870aba348_photo-4-p-1080.jpg"
               alt=""
             />
           </div>
 
-          <div className="content items-start lg:px-12 flex-1 justify-center flex flex-col gap-10">
+          <div className="content items-start lg:px-12 flex-1 justify-center flex flex-col gap-4 pt-10">
             <h2>About Kofo</h2>
-            <p className="text-xxl lg:text-3xl text-dimGray">
+            <p className="text-xxl lg:text-2xl text-dimGray">
               We are an award-winning branding and web agency committed to
               excellence since forever.
             </p>
 
-            <p className="text-xxl lg:text-3xl text-dimGray">
+            <p className="text-xxl lg:text-2xl text-dimGray">
               We set the standard of exceptional design creatively, technically,
               sustainably, and ethically so that you can make the extraordinary
               impact impact that you seek.
