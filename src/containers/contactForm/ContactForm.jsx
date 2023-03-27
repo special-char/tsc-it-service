3c659110f5bd2c54060c8c2f27c670d0be6fc118
Jil Sejpal <jilsejpal7@gmail.com>
2023-03-27 10:09:07 +0530

    commit code

--- src/containers/contactForm/ContactForm.jsx
+++ src/containers/contactForm/ContactForm.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// import { getFormDetails } from '@/lib/getFormDetails';
+// import { use } from "react";
 
-const ContactForm = () => {
-  return <div>ContactForm</div>;
+import DynamicForm from "@/components/DynamicForm/DynamicForm";
+
+// type Props = {};
+
+const ContactForm = ({ fields, submitUrl }) => {
+  // const formDetails = use(getFormDetails(1));
+
+  // if (!formDetails) {
+  //   throw new Error(
+  //     "something went wrong! try refreshing the page or please come back later."
+  //   );
+  // }
+
+  // const fields = formDetails.data?.attributes?.fields;
+
+  // const submitUrl = formDetails.data?.attributes?.submitURL as string;
+
+  return (
+    <div className="contact__body">
+      <DynamicForm fields={fields} submitUrl={submitUrl} />
+    </div>
+  );
 };
 
 export default ContactForm;
